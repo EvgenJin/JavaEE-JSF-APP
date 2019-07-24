d3166241da94a4a773a80efc9b642b6605e01da8
EvgenJin <rumbiroid@gmail.com>
2019-07-24 17:30:55 +0500

    1
    1

--- src/main/java/evg/login/Controller/VwExpCuscontroller.java
+++ src/main/java/evg/login/Controller/VwExpCuscontroller.java
@@ -2,7 +2,9 @@ package evg.login.Controller;
 
 import evg.login.Dao.VwExpCusDAO;
 import evg.login.Entity.VwExpCus;
+import evg.login.Util.JsfUtil;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
@@ -15,7 +17,53 @@ public class VwExpCuscontroller implements Serializable{
     private VwExpCusDAO dao;
     
     private List<VwExpCus> cus_list;
-   
+    private String first_name;
+    private String second_name;
+    private String third_name;
+    private String doc_ser;
+    private String doc_num;
+    
+    public String getDoc_ser() {
+        return doc_ser;
+    }
+
+    public void setDoc_ser(String doc_ser) {
+        this.doc_ser = doc_ser;
+    }
+
+    public String getDoc_num() {
+        return doc_num;
+    }
+
+    public void setDoc_num(String doc_num) {
+        this.doc_num = doc_num;
+    }
+    
+    
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getSecond_name() {
+        return second_name;
+    }
+
+    public void setSecond_name(String second_name) {
+        this.second_name = second_name;
+    }
+
+    public String getThird_name() {
+        return third_name;
+    }
+
+    public void setThird_name(String third_name) {
+        this.third_name = third_name;
+    }
+     
     public VwExpCuscontroller () {
     }
     
@@ -24,11 +72,21 @@ public class VwExpCuscontroller implements Serializable{
     }
     
     public List<VwExpCus> getCus_list() {
-        cus_list = getDao().getAll();
+//        cus_list = getDao().getAll();
+        cus_list = new ArrayList();
         return cus_list;
     }
 
     public void setCus_list(List<VwExpCus> cus_list) {
         this.cus_list = cus_list;
-    }    
+    }
+    
+    public void SearchByRecs() {
+        System.err.println(first_name);
+        System.err.println(second_name);
+        System.err.println(third_name);
+        System.err.println(doc_ser);
+        System.err.println(doc_num);
+    }
+
 }
